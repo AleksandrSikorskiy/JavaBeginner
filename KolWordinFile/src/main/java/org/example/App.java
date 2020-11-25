82e3b8c4b09c3eb19cafd463b881a7a0434f52b1
SikorskiyAG <sikorskiyag@gmail.com>
2020-11-25 20:59:42 +0200

    now commit

--- KolWordinFile/src/main/java/org/example/App.java
+++ KolWordinFile/src/main/java/org/example/App.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,23 +15,46 @@ class App {
 
     public static void getWordCount(String filename) throws FileNotFoundException{
         String[] maxarr=new String[5000];
-        File file = new File(filename);
-        Scanner scanner = new Scanner(file);
-        int imax=0;
-        String linestr="";
-        while (scanner.hasNextLine()) {
-            String[] minarr= scanner.nextLine().split(" ");
-            for (int i = 0; i < minarr.length; i++) {
-                linestr=minarr[i].trim();
-                if (!"".equals(linestr))  /**(linestr != "")**/ {
-                    maxarr[imax]=linestr;
-                    imax++;
+        int j=0;
+        for (j=0;j<5000;j++){
+            maxarr[j]="";
+        }
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            int imax = 0;
+            String linestr = "";
+            while (scanner.hasNextLine()) {
+                String[] minarr = scanner.nextLine().split(" ");
+                for (int i = 0; i < minarr.length; i++) {
+                    linestr = minarr[i].trim();
+                    if (!"".equals(linestr))  /**(linestr != "")**/ {
+                        maxarr[imax] = linestr;
+                        imax++;
+                    }
                 }
             }
+            File fileDestination = new File("c:\\Install\\grpword.txt");
+            FileWriter bufWrite = null;
+            bufWrite = new FileWriter(fileDestination);
+            Arrays.sort(maxarr);
+            int k = 0;
+            int kol = 1;
+            for (k = 0; k < 4999; k=k+1) {
+                if (maxarr[k].equals(maxarr[k + 1])) {
+                    kol = kol + 1;
+                } else {
+                    bufWrite.write(maxarr[k] +" "+ kol + "\r" + "\n");
+                    kol = 1;
+                }
+             }
+            scanner.close();
+            bufWrite.close();
+        } catch (IOException e){
+            e.printStackTrace();
         }
+       /** System.out.println(Arrays.toString(maxarr)); **/
 
-        System.out.println(Arrays.toString(maxarr));
-        scanner.close();
     }
 }
       /**  открываем  файл **/
